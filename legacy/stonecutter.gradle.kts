0810fa7ffbc132b7a33663e482a600d5305d4740
sinntika <141003113+sinntika@users.noreply.github.com>
2026-08-07 10:47:04 +0900

    Update stonecutter.gradle.kts

--- legacy/stonecutter.gradle.kts
+++ legacy/stonecutter.gradle.kts
@@ -3,21 +3,3 @@ plugins {
 }
 
 stonecutter active "1.21.1" /* [SC] DO NOT EDIT */
-
-stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
-    group = "project"
-    ofTask("build")
-}
-
-// 全バージョンをビルドして、リポジトリ直下の dist/ に jar を集める
-tasks.register<Copy>("collectJars") {
-    group = "project"
-    dependsOn("chiseledBuild")
-    from(fileTree(rootDir.resolve("versions")) {
-        include("*/build/libs/*.jar")
-        exclude("**/*-sources.jar", "**/*-dev.jar", "**/*-dev-shadow.jar")
-    })
-    into(rootDir.parentFile.resolve("dist"))
-    eachFile { path = name }
-    includeEmptyDirs = false
-}
